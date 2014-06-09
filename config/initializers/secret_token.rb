663157795379091a4c397bfbc97d6fe1ab83136a
tracymu <tracy.musung@moomumedia.com>
2014-06-09 13:01:17 +1000

    Created rails app and added spree

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Mystore::Application.config.secret_key_base = '804f36f5521aa01cc83f64bfb92015adb3ebf7d9189e141009d40cf067e50e82fa6629e8be3082d07cfe8a20df49d11a4359549620fd0b44e2b4ccdd3c288621'
